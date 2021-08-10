1664df113c62339077980231964f7e9fabd87630
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-08-10 03:18:51 +0000

    Project finished

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
 /* eslint-disable */
+// 📝 Instructions
+// Create a script that generate all the possible
+// domain name combinations from a list of pronouns,
+// adjectives and nouns, for example:
+
+// Should generate something like:
+
+// thegreatjogger.com
+// thegreatracoon.com
+// ourgreatjogger.com
+// ourgreatracoon.com
+// thebigjogger.com
+// thebigracoon.com
+// ourbigjogger.com
+// ourbigracoon.com
+
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +23,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  domainGen();
+};
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let name = [];
+
+const p = document.querySelector("#domain");
+const lista = document.createElement("ul");
+
+const domainGen = () => {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        name.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+
+  for (let item of name) {
+    let li = document.createElement("li");
+    let text = document.createTextNode(item);
+    li.appendChild(text);
+    lista.appendChild(li);
+  }
+
+  p.appendChild(lista);
 };
